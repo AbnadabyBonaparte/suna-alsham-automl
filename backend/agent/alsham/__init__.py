7ee43cf1867716c4c1fc7c41982a2c4dfd2d4b09
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-07-20 09:34:32 -0300

    Create __init__.py

---
+++ backend/agent/alsham/__init__.py
@@ -0,0 +1,11 @@
+"""
+SUNA-ALSHAM Core Package
+Core Agent AutoML e componentes principais
+"""
+
+__version__ = "2.1.0"
+
+from .core_agent import CoreAgent, CoreAgentConfig
+
+__all__ = ["CoreAgent", "CoreAgentConfig"]
+
