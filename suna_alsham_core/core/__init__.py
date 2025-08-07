5ab5d46bdabadde9b4b1baceccafb931684b8a2e
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-08-07 15:24:54 -0300

    Create __init__.py

---
+++ suna_alsham_core/core/__init__.py
@@ -0,0 +1,5 @@
+"""ALSHAM QUANTUM - Core Package"""
+from .base_agent import BaseAgent
+from .quantum_core import QuantumCore, quantum_core, get_core, initialize_core
+
+__all__ = ['BaseAgent', 'QuantumCore', 'quantum_core', 'get_core', 'initialize_core']
