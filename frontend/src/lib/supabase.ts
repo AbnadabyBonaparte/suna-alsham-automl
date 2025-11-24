cbd83eff8f31fc326da3cde35bc4ea76a7a300ca
AbnadabyBonaparte <abnadabybonaparte@gmail.com>
2025-11-23 22:28:23 -0300

    Update supabase.ts

--- frontend/src/lib/supabase.ts
+++ frontend/src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Hardcode temporário para garantir funcionamento
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vktzdrsigrdnemdshcdp.supabase.co';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrdHpkcnNpZ3JkbmVtZHNoY2RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4MzMyODksImV4cCI6MjA2ODQwOTI4OX0.W5n4HbmQqUcGe_tmRPBBfiDhVWcDwK6KF8FrQiR11jc';
 
